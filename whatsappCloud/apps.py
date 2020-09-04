364edb80df578c0f2d1e23bc33d0221f0579fa24
emibarrod <eb@athento.com>
2020-09-04 13:07:16 +0200

    refactor

--- whatsappCloud/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WhatsappcloudConfig(AppConfig):
-    name = 'whatsappCloud'
---
+++ whatsappWordCloud/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WhatsappWordCloudConfig(AppConfig):
+    name = 'whatsappWordCloud'
