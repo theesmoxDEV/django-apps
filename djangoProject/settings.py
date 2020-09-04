7815caa9992ac793a2e0508ecf26490623bf36a7
emibarrod <eb@athento.com>
2020-09-04 13:01:48 +0200

    fixed SECRET_KEY

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -22,7 +22,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'foo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
